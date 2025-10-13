c77525d3ed9b1c44630b75bac4dd5505b9b0e6e4
Vighnesh SL <vighnesh55642@gmail.com>
2025-10-13 17:40:23 +0530

    updated env to in production

--- src/enviorments/environment.prod.ts
+++ src/enviorments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  api: 'https://vortex-fit.space',
+  api: '/api',
   googleClientId:
     '414745868113-lpndqpgeruk3c6493j18lfkmg0v103me.apps.googleusercontent.com',
   razorpayKey: 'rzp_test_OZVJGxiPWQgDaM',
   adminId: '684c36b322e80a1b938b0a99',
-  googleUrl: 'https://accounts.google.com/o/oauth2/v2/auth',
+  googleUrl: '/auth/google/redirect',
 };
--- src/enviorments/environment.ts
+++ src/enviorments/environment.ts
@@ -5,5 +5,5 @@ export const environment = {
     '414745868113-lpndqpgeruk3c6493j18lfkmg0v103me.apps.googleusercontent.com',
   razorpayKey: 'rzp_test_OZVJGxiPWQgDaM',
   adminId: '684c36b322e80a1b938b0a99',
-  googleUrl: 'https://accounts.google.com/o/oauth2/v2/auth'
+  googleUrl: 'https://accounts.google.com/o/oauth2/v2/auth',
 };
