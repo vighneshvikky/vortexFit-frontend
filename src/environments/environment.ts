292a5bffce89da121796333951569726873fb536
Vighnesh SL <vighnesh55642@gmail.com>
2025-10-16 10:40:16 +0530

    updated socket service to get right api end point

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   api: 'http://localhost:3000',
+  socketUrl: 'http://localhost:3000',
   googleClientId:
     '414745868113-lpndqpgeruk3c6493j18lfkmg0v103me.apps.googleusercontent.com',
   razorpayKey: 'rzp_test_OZVJGxiPWQgDaM',
